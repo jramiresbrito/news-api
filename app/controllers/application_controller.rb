09d402afe4e14cab7584f71121608eeb520b8465
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-10-29 17:33:31 -0300

    Finaliza a API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
-require "application_responder"
+require 'application_responder'
 
 class ApplicationController < ActionController::API
-  self.responder = ApplicationResponder
-  respond_to :html
+  before_action :set_flash_only_for_html
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+  self.responder = ApplicationResponder
+  respond_to :json
+
+  private
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def set_flash_only_for_html
+    request.format = :json unless request.format.html?
+  end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,11 @@
 class News < ApplicationRecord
   belongs_to :user
+  validates :likes, numericality: { greater_than_or_equal_to: 0 }
+  before_validation :set_default_likes, on: :create
+
+  private
+
+  def set_default_likes
+    self.likes ||= 0
+  end
 end
