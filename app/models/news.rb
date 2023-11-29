b9e4713a63ad23ead98bd15720b0e057fab5b052
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2023-11-28 23:45:27 -0300

    Update news controller to return user data

--- app/models/news.rb
+++ app/models/news.rb
@@ -3,6 +3,10 @@ class News < ApplicationRecord
   validates :likes, numericality: { greater_than_or_equal_to: 0 }
   before_validation :set_default_likes, on: :create
 
+  def as_json(options = {})
+    super(options.merge(include: { user: { only: %i[id email] } }))
+  end
+
   private
 
   def set_default_likes
